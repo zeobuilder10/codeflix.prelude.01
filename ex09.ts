9b4e27f7d2a10f63a244913966d4c83eaa4a778b
zeobuilder10 <adrienclemenceau1@gmail.com>
2021-10-04 11:52:25 +0200

    Adding ex09.ts

---
+++ ex09.ts
@@ -0,0 +1,9 @@
+function charAtP(str='', position: number): string { // :) //
+    return str.charAt(position) // :) //
+} // :) //
+
+// :) //
+
+console.log(charAtP('chopper', 0)) // :) //
+
+console.log(charAtP('chopper', 1)) // :) //
